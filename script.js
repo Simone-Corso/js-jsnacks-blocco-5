5f86673f6de728fab3a641d078b75f9c171a50ec
Simone-Corso <scorso26@gmail.com>
2023-11-27 12:17:14 +0100

    inserito un array con 4 persone

--- script.js
+++ script.js
@@ -3,3 +3,31 @@
 
 //Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
 
+
+const containerElement = document.querySelector('main > section.lista');
+
+const listaPersone = [
+    {
+        name: 'Simone',
+        lastname: 'gino',
+        age: '54',
+    },
+    {
+        name: 'Federico',
+        lastname: 'rico',
+        age: '90',
+    },
+
+{
+    name: 'Jonny',
+    lastname: 'bar',
+    age: '78',
+},
+
+{
+    name: 'Goku',
+    lastname: 'potenza',
+    age: '23',
+},
+
+];
