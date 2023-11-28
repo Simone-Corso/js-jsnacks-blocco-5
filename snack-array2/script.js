5bbd779ac70ffbcea3ea5ac74e4a7fa720aa7ef2
Simone-Corso <scorso26@gmail.com>
2023-11-28 12:24:04 +0100

    inserito cartella

---
+++ snack-array2/script.js
@@ -0,0 +1,20 @@
+//Un oggetto rappresenta una finestra di un browser e contiene due campi: un array di tab aperte e un numero che indica l'indice della tab aperta nell'array:
+//{
+//    "tab": ["Facebook", "GitHub", "Gmail"],
+  //  "activeTab": 0
+//}
+//Il software deve guardare se c'è un social aperto ed eliminarlo dalle tab.
+//Nel caso la tab fosse attiva, deve attivare la successiva.
+
+const tab = {
+    tab: ["Facebook", "GitHub", "Gmail"],
+    activeTab: 1
+}
+console.log(tab);
+
+tab.splice(activeTab,1);
+
+//dovremmo eliminare quello che è attivo e pusharlo nell'array eliminato
+
+const = ['facebook','twiter', 'github'];
+
